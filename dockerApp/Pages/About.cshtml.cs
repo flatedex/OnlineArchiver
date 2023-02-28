8af77960fba4b537039ecdbad2b7695a15a4dade
flatedex <flatedex@mail.ru>
2023-02-28 18:53:06 +0300

    Html and css sources migrated on ASP.NET Core

---
+++ dockerApp/Pages/About.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace dockerApp.Pages.Shared
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ dockerApp/Pages/Login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace dockerApp.Pages
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
